aef0b1765cd1e431ee764a2bae77677745215669
Brad <bgreen3@uoregon.edu>
2016-12-11 21:25:42 -0700

    Add conditional for user not belonging to any organizations

--- spec/features/user_logs_in_with_github_spec.rb
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -1,11 +1,35 @@
 require "rails_helper"
+require "launchy"
 
 describe "Login" do
   context "User visits root_path" do
-    xit "can log in to Github" do
+    it "can log in to Github" do
+      OmniAuth.config.test_mode = true
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+        provider: 'github',
+        credentials: {token: '1234'},
+        extra: {
+          raw_info: {
+            login: 'fakeuser',
+            avatar_url: 'www.fakeurl.com',
+            followers: 4,
+            following: 3
+          }
+        }
+      })
+
+      user = User.new(uid: '1234',
+                      username: 'fakeuser',
+                      token: '1234',
+                      avatar_url: 'www.fakeurl.com',
+                      followers: 4,
+                      following: 3)
+
       visit root_path
-      click_button 'Login'
-      expect(page).to have_content("")
+      click_button "Login"
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Followers: 4")
     end
   end
 end
